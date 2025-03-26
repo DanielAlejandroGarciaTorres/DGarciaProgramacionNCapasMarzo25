2c42069db36086d7d035df9a0286e7013afe457c
Alejandro García <>
2025-03-26 12:10:21 -0600

    Spring project

---
+++ src/main/java/com/digis01/DGarciaPorgramacionNCapasMarzo25/Controller/AlumnoController.java
@@ -0,0 +1,21 @@
+package com.digis01.DGarciaPorgramacionNCapasMarzo25.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Alumno")
+public class AlumnoController {
+    
+   // Model y ModelAttribute
+    @GetMapping
+    public String Index(Model model){
+        
+        String nombre  = "Jesus";
+        model.addAttribute("nombre", nombre);
+        
+        return "AlumnoIndex";
+    }
+}
