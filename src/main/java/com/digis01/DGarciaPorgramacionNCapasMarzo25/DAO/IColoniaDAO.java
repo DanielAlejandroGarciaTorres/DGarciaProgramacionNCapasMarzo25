df1cbfd2a8e83679c61b117b2621e1829594f5d1
digis <digis@ALIEN21>
2025-04-07 18:37:01 -0600

    Estado DDL

---
+++ src/main/java/com/digis01/DGarciaPorgramacionNCapasMarzo25/DAO/IColoniaDAO.java
@@ -0,0 +1,9 @@
+package com.digis01.DGarciaPorgramacionNCapasMarzo25.DAO;
+
+import com.digis01.DGarciaPorgramacionNCapasMarzo25.ML.Result;
+
+
+public interface IColoniaDAO {
+        
+    Result ColoniaByIdMunicipio(int IdMunicipio);
+}
